c8232dc03a0ba3a77c2e30d1f8bd697fcc9f3d44
tagupta <guptatanukhandelwal.96@gmail.com>
2022-09-09 09:26:00 +0530

    feat: file based routing

---
+++ pages/events/[...slug].js
@@ -0,0 +1,9 @@
+const FilteredEventsPage = () => {
+  return (
+    <div>
+      <h1>Display a page filtered by some parameters</h1>
+    </div>
+  );
+};
+
+export default FilteredEventsPage;
---
+++ pages/events/[eventId].js
@@ -0,0 +1,9 @@
+const EventDetailPage = () => {
+  return (
+    <div>
+      <h1>Rendering a page for specific Event Id</h1>
+    </div>
+  );
+};
+
+export default EventDetailPage;
