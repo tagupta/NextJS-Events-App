ce82df24749bb478eecd82e5507ddbc1e73149cf
tagupta <guptatanukhandelwal.96@gmail.com>
2022-09-12 07:46:41 +0530

    feat: client side prefetching

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,16 +1,97 @@
-import { Fragment } from "react";
+import { useRouter } from "next/router";
+import { Fragment, useEffect, useState } from "react";
 import EventList from "../../components/events/event-list";
 import ResultsTitle from "../../components/events/results-title";
 import Button from "../../components/ui/button";
 import { getFilteredEvents } from "../../helper/api-util";
+import useSWR from "swr";
 
-export async function getServerSideProps(context) {
-  const { params } = context;
+// export async function getServerSideProps(context) {
+//   const { params } = context;
 
-  const filteredData = params.slug;
-  if (!filteredData) {
+//   const filteredData = params.slug;
+//   if (!filteredData) {
+//     return <p className="center">Loading...</p>;
+//   }
+//   const filteredYear = filteredData[0];
+//   const filteredMonth = filteredData[1];
+
+//   const numYear = +filteredYear;
+//   const numMonth = +filteredMonth;
+
+//   if (
+//     isNaN(numYear) ||
+//     isNaN(numMonth) ||
+//     numYear > 2030 ||
+//     numYear < 2021 ||
+//     numMonth > 12 ||
+//     numMonth < 1
+//   )
+//     return {
+//       props: {
+//         hasError: true,
+//       },
+//       // notFound: true,
+//       // redirect: {
+//       //   destination: "/error",
+//       // },
+//     };
+
+//   let filteredEvents = await getFilteredEvents({
+//     year: numYear,
+//     month: numMonth,
+//   });
+
+//   return {
+//     props: {
+//       filteredEvents,
+//       date: {
+//         year: numYear,
+//         month: numMonth,
+//       },
+//     },
+//   };
+// }
+
+const fetcher = async (url) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
+
+const FilteredEventsPage = (props) => {
+  const [loadedEvents, setLoadedEvents] = useState();
+  const router = useRouter();
+  const filteredData = router.query.slug;
+
+  const { data, error } = useSWR(
+    "https://nextjs-course-41eb5-default-rtdb.asia-southeast1.firebasedatabase.app/events.json",
+    fetcher
+  );
+
+  useEffect(() => {
+    if (data) {
+      const events = [];
+
+      for (const key in data) {
+        events.push({
+          id: key,
+          ...data[key],
+        });
+      }
+
+      setLoadedEvents(events);
+    }
+  }, [data]);
+
+  if (error) {
+    return <p className="center">Failed to load.</p>;
+  }
+
+  if (!loadedEvents || !filteredData) {
     return <p className="center">Loading...</p>;
   }
+
   const filteredYear = filteredData[0];
   const filteredMonth = filteredData[1];
 
@@ -24,34 +105,7 @@ export async function getServerSideProps(context) {
     numYear < 2021 ||
     numMonth > 12 ||
     numMonth < 1
-  )
-    return {
-      props: {
-        hasError: true,
-      },
-      // notFound: true,
-      // redirect: {
-      //   destination: "/error",
-      // },
-    };
-
-  let filteredEvents = await getFilteredEvents({
-    year: numYear,
-    month: numMonth,
-  });
-
-  return {
-    props: {
-      filteredEvents,
-      date: {
-        year: numYear,
-        month: numMonth,
-      },
-    },
-  };
-}
-const FilteredEventsPage = (props) => {
-  if (props.hasError) {
+  ) {
     return (
       <Fragment>
         <p className="center">Invalid filter. Please adjust your values.</p>
@@ -62,7 +116,14 @@ const FilteredEventsPage = (props) => {
     );
   }
 
-  const filteredEvents = props.filteredEvents;
+  let filteredEvents = loadedEvents.filter((event) => {
+    const eventDate = new Date(event.date);
+    return (
+      eventDate.getFullYear() === numYear &&
+      eventDate.getMonth() === numMonth - 1
+    );
+  });
+
   if (!filteredEvents || filteredEvents.length === 0) {
     return (
       <Fragment>
@@ -74,7 +135,7 @@ const FilteredEventsPage = (props) => {
     );
   }
 
-  const date = new Date(props.date.year, props.date.month - 1);
+  const date = new Date(numYear, numMonth - 1);
 
   return (
     <div>
